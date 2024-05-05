62c4beaea65d09cfae35d9857441c17fa6a1a401
Quan Nguyen <157680058+quan-nguyen-2@users.noreply.github.com>
2024-05-05 13:23:55 -0500

    Add files via upload

---
+++ flask-greet-calc/calc/app.py
@@ -0,0 +1,34 @@
+# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+    return str(result)
+
+
+@app.route('/sub')
+def sub(a, b):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+    return str(result)
+
+@app.route('/mult')
+def mult(a, b):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+    return str(result)
+
+@app.route('/div')
+def add(a, b):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+    return str(result)
